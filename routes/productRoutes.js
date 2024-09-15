a8236d18ac40ec591194067047d33190cb19a350
Jyotiprakash-01 <jyotiprakashtripathy2004@gmail.com>
2024-09-15 17:25:56 +0530

    adding product routes to add and retrieve products

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const { addProduct,getProducts } = require('../controllers/productController');
+const auth = require('../middleware/auth');
+const router = express.Router();
+router.use(cors());
+// Add a new product
+router.post('/add', auth, addProduct);
+// Get all products
+router.get('/retrieve', getProducts);
+
+module.exports = router;
 
