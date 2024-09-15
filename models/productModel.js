2d79a40a1e26d5261caed41c72e2d2d5d0fbfb15
Jyotiprakash-01 <jyotiprakashtripathy2004@gmail.com>
2024-09-15 14:26:09 +0530

    removing product

--- models/productModel.js
+++
@@ -1,3 +0,0 @@
-
-module.exports = mongoose.model('Product', ProductSchema);
-
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,10 +1,1 @@
-// this route handles post requests by the user to add aproduct it validates an user from the token assigned to it using auth of middleware
-const express = require('express');
-const { addProduct } = require('../controllers/productController');
-const auth = require('../middleware/auth');
-const router = express.Router();
 
-// Add a new product
-router.post('/add', auth, addProduct);
-
-module.exports = router;
