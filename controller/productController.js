6b990ec7c4b5cef586a9bdd40ee10da9a1e6b2b5
Jyotiprakash-01 <jyotiprakashtripathy2004@gmail.com>
2024-09-15 14:02:28 +0530

    removing the addproduct model from the product controllerjs

--- controller/productController.js
+++ controller/productController.js
@@ -1,18 +1,1 @@
 
-exports.addProduct = async (req, res) => {
-  const { name, description, price, quantity } = req.body;
-  try {
-    const newProduct = new Product({
-      farmer: req.user.id,
-      name,
-      description,
-      price,
-      quantity,
-    });
-    const product = await newProduct.save();
-    res.json(product);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server error');
-  }
-};
