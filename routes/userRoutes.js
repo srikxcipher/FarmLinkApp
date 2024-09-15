147c6eeb9c8330d54c5710342ac1ff9be008a7ea
Jyotiprakash-01 <jyotiprakashtripathy2004@gmail.com>
2024-09-15 16:29:25 +0530

    adding a test route and enabling cors

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,3 +6,9 @@ const authenticateToken = require('../middleware/profile');
 const User = require('../models/userModel');
 
 const router = express.Router();
+// Enable CORS for this specific router
+router.use(cors());
+// Test route to check if routing is working
+router.get('/test', (req, res) => {
+    res.send('Test route is working!');
+  });
