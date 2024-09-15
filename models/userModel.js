ae36998f168df24660cf14c6ec9da11b0796bb35
Jyotiprakash-01 <jyotiprakashtripathy2004@gmail.com>
2024-09-15 15:05:35 +0530

    defining the user schema

--- models/userModel.js
+++ models/userModel.js
@@ -1,2 +1,10 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ['farmer', 'consumer', 'retailer'], required: true },
+  date: { type: Date, default: Date.now },
+});
