e8b5ea0f91b5d17c1842c34eb044c1e26dbcdac0
Jyotiprakash-01 <jyotiprakashtripathy2004@gmail.com>
2024-09-15 14:22:41 +0530

    removing the product schema

--- models/productModel.js
+++ models/productModel.js
@@ -1,21 +1,3 @@
 
-const ProductSchema = new mongoose.Schema({
-  farmer: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  name: { type: String, required: true },
-  description: { type: String },
-  price: { type: Number, required: true, min: 0 }, // Ensure price is non-negative
-  quantity: { type: Number, required: true, min: 0 }, // Ensure quantity is non-negative
-  dateAdded: { type: Date, default: Date.now },
-});
-
-// Indexes
-ProductSchema.index({ farmer: 1 });
-ProductSchema.index({ name: 1 });
-
-// Instance Method to Check Stock
-ProductSchema.methods.isInStock = function () {
-  return this.quantity > 0;
-};
-
 module.exports = mongoose.model('Product', ProductSchema);
 
