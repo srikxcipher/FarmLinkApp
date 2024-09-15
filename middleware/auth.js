c1dc5c1f035edb45b86ab5119de29a9a0488f2d2
Jyotiprakash-01 <jyotiprakashtripathy2004@gmail.com>
2024-09-15 14:04:33 +0530

    removing the export model from the auth middle ware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,21 +1,4 @@
 
 
-module.exports = function (req, res, next) {
-  // Retrieve token from Authorization header
-  const authHeader = req.header('Authorization');
-  const token = authHeader && authHeader.startsWith('Bearer ') ? authHeader.substring(7) : null;
 
-  if (!token) {
-    return res.status(401).json({ msg: 'No token, authorization denied' });
-  }
-
-  try {
-    // Verify the token
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded.user;
-    next();
-  } catch (err) {
-    res.status(401).json({ msg: 'Token is not valid' });
-  }
-};
 
